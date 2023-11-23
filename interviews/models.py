db48c2e120e02d07afa1ac3e22e7a60be453a0a9
Italo Ramon Lima Cunha <italo.lima@VALLE.br>
2023-11-22 22:13:08 -0300

    first commit

---
+++ interviews/models.py
@@ -0,0 +1,68 @@
+import uuid
+from django.db import models
+from django.conf import settings
+from .services import GptService
+
+
+# Create your models here.
+
+class Chat(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200, editable=False)
+    job = models.ForeignKey("jobs.Job", on_delete=models.CASCADE, related_name="chats")
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.uuid = uuid.uuid4()
+        self.title = f"Chat {self.job.title} - {self.uuid}"
+        super().save(*args, **kwargs)
+        initial_prompt = settings.INITIAL_PROMPT_TEMPLATE
+        initial_prompt = initial_prompt.replace("{job_title}", self.job.title)
+        initial_prompt = initial_prompt.replace("{job_requirements}", self.job.requirements)
+        initial_prompt = initial_prompt.replace("{job_responsibilities}", self.job.responsibilities)
+        Message.objects.create(
+            chat=self,
+            role="system",
+            content=initial_prompt
+        )
+        super().save(*args, **kwargs)
+
+
+class Message(models.Model):
+    ROLE_CHOICES = (
+        ("system", "Sistema"),
+        ("user", "Candidato"),
+        ("assistant", "Entrevistador")
+    )
+    chat = models.ForeignKey("interviews.Chat", on_delete=models.CASCADE, related_name="messages")
+    role = models.CharField(max_length=9, choices=ROLE_CHOICES)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.role} - {self.chat.title}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk and self.role != "assistant" and not self.chat.completed:
+            super().save(*args, **kwargs)
+
+            if self.role == "user" and self.chat.messages.filter(role="assistant").count() >= 5:
+                Message.objects.create(
+                    chat=self.chat,
+                    role="system",
+                    content="Realize o feedback do candidato, esse feedback deve informar quais foram os pontos positivos, os pontos negativos, o que pode ser melhorado e se o canditado possui o perfil adequado para a vaga."
+                )
+                self.chat.completed = True
+                self.chat.save()
+            else:
+                service = GptService()
+                Message.objects.create(
+                    chat=self.chat,
+                    role="assistant",
+                    content=service.get_chat_message(self.chat.messages.all())
+                )
+        else:
+            super().save(*args, **kwargs)
---
+++ jobs/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+
+class Job(models.Model):
+    LEVEL_CHOICES = (
+        ('jr','Júnior'),
+        ('pl','Pleno'),
+        ('sr','Sênior'),
+    )
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    requirements = models.TextField()
+    responsibilities = models.TextField()
+    level = models.CharField(max_length=2,choices=LEVEL_CHOICES)
+    skills = models.ManyToManyField("jobs.Skill",related_name="jobs")
+
+    def __str__(self):
+        return self.title
+
+    def requirements_list(self):
+        return self.requirements.split("\n")
+
+    def responsibilities_list(self):
+        return self.responsibilities.split("\n")
+
+
+class Skill(models.Model):
+    title = models.CharField(max_length=100,unique=True,blank=False,null=False)
+    
+    def __str__(self):
+        return self.title
+
